191c08a405fca61bb8c9cc179099485b204c48f4
Camila <camila.matosrocha1234@gmail.com>
2023-11-14 20:31:49 -0300

    feat: create left-panel component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,30 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor(
-    private spotifyService: SpotifyService
-  ) { }
+  constructor(private spotifyService: SpotifyService, private router: Router) {}
 
   ngOnInit(): void {
     this.checkTokenUrlCallback();
   }
 
-  checkTokenUrlCallback() {
+  public checkTokenUrlCallback() {
     const token = this.spotifyService.getTokenCallbackUrl();
     if (!!token) {
       this.spotifyService.setAccessToken(token);
+      this.router.navigate(['/player']);
     }
   }
 
   public login() {
     window.location.href = this.spotifyService.getLoginUrl();
   }
-
 }
--- src/app/pages/player/player.module.ts
+++ src/app/pages/player/player.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { PlayerComponent } from './player.component';
 import { RouterModule } from '@angular/router';
 import { PlayerRoutes } from './player.routes';
+import { LeftPanelComponent } from 'src/app/components/left-panel/left-panel.component';
 
 @NgModule({
   declarations: [
-    PlayerComponent
+    PlayerComponent,
+    LeftPanelComponent
   ],
   imports: [
     CommonModule,
